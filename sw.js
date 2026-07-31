bffcbbc16e575ee7a426f69a88286e95d940ac64
Alberto <r@mirez.com>
2026-07-31 00:02:30 -0400

    new icon and fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this when you change index.html so devices pick up the new version
-const CACHE_NAME = "learning-pad-v3";
+const CACHE_NAME = "learning-pad-v4";
 const ASSETS = [
   "./",
   "./index.html",
